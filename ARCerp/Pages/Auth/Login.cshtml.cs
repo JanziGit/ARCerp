0175f562653305dcc267647f744f438c02546c4d
Jahanzaib-Ali <jahanzaib@microsysx.com>
2024-06-05 05:15:54 +0500

    Production manager page changes

--- ARCerp/Pages/Auth/Login.cshtml.cs
+++ ARCerp/Pages/Auth/Login.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ARCerp.Pages.UserAccount
+namespace ARCerp.Pages.Auth
 {
     public class LoginModel : PageModel
     {
